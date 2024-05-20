9b609851ae460bfcde3d6a56cb465ad5a447641f
victoria.clube <victoria.clube@gmail.com>
2024-05-19 21:22:45 -0300

    projetoguiado2gestãoPJ

--- Exercicios js/class.js
+++
@@ -1,9 +0,0 @@
-class biblioteca {
-    constructor(titulo, autor, quantidade) {
-        this._titulo = titulo;
-        this.autor = autor;
-        this.__quantidade = quantidade;
-    }
-
-    // Outros métodos da classe
-}
